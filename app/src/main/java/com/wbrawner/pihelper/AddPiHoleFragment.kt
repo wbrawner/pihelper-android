66838ed4a2ec7813123b64ef767d0448699d0f1c
William Brawner <me@wbrawner.com>
2020-02-05 08:18:01 -0600

    Fix lint warnings

--- app/src/main/java/com/wbrawner/pihelper/AddPiHoleFragment.kt
+++ app/src/main/java/com/wbrawner/pihelper/AddPiHoleFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_add_pi_hole.*
--- app/src/main/java/com/wbrawner/pihelper/PiHelperApplication.kt
+++ app/src/main/java/com/wbrawner/pihelper/PiHelperApplication.kt
@@ -11,6 +11,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
+@Suppress("unused")
 @AcraCore(buildConfigClass = BuildConfig::class)
 @AcraMailSender(mailTo = "pihelper@wbrawner.com")
 @AcraNotification(
