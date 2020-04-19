eebb3b7699713513eddbd63428a216e683f9c4f5
William Brawner <me@wbrawner.com>
2020-04-19 10:40:09 -0700

    Remove automated error loggers

--- app/src/main/java/com/wbrawner/pihelper/PiHelperApplication.kt
+++ app/src/main/java/com/wbrawner/pihelper/PiHelperApplication.kt
@@ -3,23 +3,11 @@ package com.wbrawner.pihelper
 import android.app.Application
 import android.content.Context
 import com.wbrawner.piholeclient.piHoleClientModule
-import org.acra.ACRA
-import org.acra.annotation.AcraCore
-import org.acra.annotation.AcraMailSender
-import org.acra.annotation.AcraNotification
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 @Suppress("unused")
-@AcraCore(buildConfigClass = BuildConfig::class)
-@AcraMailSender(mailTo = "pihelper@wbrawner.com")
-@AcraNotification(
-    resIcon = R.drawable.ic_notification,
-    resTitle = R.string.title_crash_notification,
-    resText = R.string.text_crash_notification,
-    resChannelName = R.string.channel_crash_notification
-)
 class PiHelperApplication: Application() {
     override fun onCreate() {
         super.onCreate()
@@ -31,10 +19,5 @@ class PiHelperApplication: Application() {
                 piHelperModule
             ))
         }
-    }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-        ACRA.init(this)
     }
 }
